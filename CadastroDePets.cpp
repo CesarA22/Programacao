d9f5fb40d815f2ac63b65bbb9f395463c4d6ae2b
Cesar Augusto <62781403+CesarA22@users.noreply.github.com>
2021-11-24 16:46:07 -0300

    Update CadastroDePets.cpp

--- CadastroDePets.cpp
+++ CadastroDePets.cpp
@@ -1,15 +1,3 @@
-/*
-Artur Henrique Pagno - 21013037
-Carlos Eduardo Fontes Camacho  - 21008649
-Cesar Augusto Camote Inocencio - 20018446
-Felipe de Moura Tayar - 201019634
-João Victor Moreira Vidal - 19291384
-Patrick Pimentel Corrêa Leite - 21007850
-
-Opções: 1, 2 e 3.
-*/
-
-
 
 #include <stdio.h>
 #include <stdlib.h>
